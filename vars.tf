e6125aa10ad16bb1d08a2d0f6cc6b78678e9619a
ravichandra.kalluri <kalluriravi444@gmail.com>
2023-04-05 17:34:44 +0530

    apps jobs github link update

--- vars.tf
+++ vars.tf
@@ -6,12 +6,12 @@ variable "jobs" {
 
   default = [
     { name = "roboshop", folder = "infra" , repo_url= "https://github.com/ravi568/roboshop-infra" },
-    { name = "frontend", folder = "CI-Pipeline" , repo_url= "https://github.com/ravi568/roboshop-infra" },
-    { name = "cart", folder = "CI-Pipeline" , repo_url= "https://github.com/ravi568/roboshop-infra" },
-    { name = "catalogue", folder = "CI-Pipeline" , repo_url= "https://github.com/ravi568/roboshop-infra" },
-    { name = "user", folder = "CI-Pipeline" , repo_url= "https://github.com/ravi568/roboshop-infra" },
-    { name = "shipping", folder = "CI-Pipeline" , repo_url= "https://github.com/ravi568/roboshop-infra" },
-    { name = "payment", folder = "CI-Pipeline" , repo_url= "https://github.com/ravi568/roboshop-infra" }
+    { name = "frontend", folder = "CI-Pipeline" , repo_url= "https://github.com/ravi568/frontend" },
+    { name = "cart", folder = "CI-Pipeline" , repo_url= "https://github.com/ravi568/cart" },
+    { name = "catalogue", folder = "CI-Pipeline" , repo_url= "https://github.com/ravi568/catalogue" },
+    { name = "user", folder = "CI-Pipeline" , repo_url= "https://github.com/ravi568/user" },
+    { name = "shipping", folder = "CI-Pipeline" , repo_url= "https://github.com/ravi568/shipping" },
+    { name = "payment", folder = "CI-Pipeline" , repo_url= "https://github.com/ravi568/payment" }
 
     ]
 }
