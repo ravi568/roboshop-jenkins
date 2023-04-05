765d2f8e9173fbbabd3388c9319bc6692840586e
ravichandra.kalluri <kalluriravi444@gmail.com>
2023-04-05 16:18:57 +0530

    components under ci-pipeline

--- vars.tf
+++ vars.tf
@@ -1,11 +1,17 @@
 variable "folders"{
-  default = ["infra"]
+  default = ["infra", "CI-Pipeline"]
 }
 
 variable "jobs" {
 
   default = [
-    { name = "roboshop", folder = "infra" , repo_url= "https://github.com/ravi568/roboshop-infra" }
+    { name = "roboshop", folder = "infra" , repo_url= "https://github.com/ravi568/roboshop-infra" },
+    { name = "frontend", folder = "CI-Pipeline" , repo_url= "https://github.com/ravi568/roboshop-infra" },
+    { name = "cart", folder = "CI-Pipeline" , repo_url= "https://github.com/ravi568/roboshop-infra" },
+    { name = "catalogue", folder = "CI-Pipeline" , repo_url= "https://github.com/ravi568/roboshop-infra" },
+    { name = "user", folder = "CI-Pipeline" , repo_url= "https://github.com/ravi568/roboshop-infra" },
+    { name = "shipping", folder = "CI-Pipeline" , repo_url= "https://github.com/ravi568/roboshop-infra" },
+    { name = "payment", folder = "CI-Pipeline" , repo_url= "https://github.com/ravi568/roboshop-infra" }
 
     ]
 }
