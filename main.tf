54906f3a6af3279c1d44ed9708179af769d81f6d
ravichandra.kalluri <kalluriravi444@gmail.com>
2023-04-07 16:55:41 +0530

    sonarqube pass parameter

--- main.tf
+++ main.tf
@@ -12,6 +12,8 @@ resource "jenkins_job" "job" {
 
   template = templatefile("${path.module}/sb-job.xml", {
     repo_url= lookup(element(var.jobs, count.index), "repo_url", null)
+    name= lookup(element(var.jobs, count.index), "name", null)
+
   })
   lifecycle {
     ignore_changes = [template]
