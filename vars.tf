2cdf4083a22a8025ddff166aa9aed18af046bf09
ravichandra.kalluri <kalluriravi444@gmail.com>
2023-05-08 11:16:09 +0530

    aws-ssm in ci pipeline vars tf in roboshop jenkins

--- vars.tf
+++ vars.tf
@@ -10,7 +10,8 @@ variable "m-jobs" {
     { name = "catalogue", folder = "CI-Pipeline" , repo_url= "https://github.com/ravi568/catalogue" },
     { name = "user", folder = "CI-Pipeline" , repo_url= "https://github.com/ravi568/user" },
     { name = "shipping", folder = "CI-Pipeline" , repo_url= "https://github.com/ravi568/shipping" },
-    { name = "payment", folder = "CI-Pipeline" , repo_url= "https://github.com/ravi568/payment" }
+    { name = "payment", folder = "CI-Pipeline" , repo_url= "https://github.com/ravi568/payment" },
+    { name = "aws-ssm-param-store", folder = "CI-Pipeline" , repo_url= "https://github.com/ravi568/aws-ssm-param-store" }
 
     ]
 }
